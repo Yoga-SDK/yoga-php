89237de49dcbba0f38649a35cbfb20277720c557
Gustavo Vilas Bôas <gustavo.boas@startaideia.com.br>
2020-01-31 21:16:04 -0300

    Initial commit

---
+++ src/YogaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Yoga;
+
+use Illuminate\Support\ServiceProvider;
+
+class YogaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        dd('aqui');
+    }
+}
